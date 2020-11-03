f79c587f27db208d4d7cd08102243913f4a33c37
Oktay Bahceci <oktaybahceci@MacBook-Pro.lan>
2020-11-03 20:54:08 +0100

    Updates to background poller.

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -11,22 +11,17 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
 public class BackgroundPoller {
-  private final WebClient client;
 
   private Logger LOGGER = LoggerFactory.getLogger(BackgroundPoller.class);
   private static String URL = "url";
   private static String STATUS = "status";
   private static String OK = "OK";
   private static String FAIL = "FAIL";
-  private final Vertx vertx;
-  public BackgroundPoller(Vertx vertx) {
-    this.client = WebClient.create(vertx);
-    this.vertx = vertx;
-  }
 
   private DBConnector dbConnector = new DBConnector(Vertx.vertx());
 
-  public Future<List<JsonObject>> pollServices(List<JsonObject> services) {
+  public Future<List<JsonObject>> pollServices(List<JsonObject> services, Vertx vertx) {
+    WebClient client = WebClient.create(vertx);
     for(JsonObject service : services) {
       String url = (String)service.getValue("url");
       LOGGER.info("Trying to poll %s", url);
--- src/main/java/se/kry/codetest/MainVerticle.java
+++
@@ -1,8 +0,0 @@
-package se.kry.codetest;
-
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-
-public class MainVerticle {
-
-    }
