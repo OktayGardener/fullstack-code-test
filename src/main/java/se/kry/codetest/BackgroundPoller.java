460cf37acfb1b8ce871698076f54519024676647
Oktay Bahceci <oktaybahceci@MacBook-Pro.lan>
2020-11-03 19:56:48 +0100

    Implement background poller

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -3,13 +3,46 @@ package se.kry.codetest;
 import io.vertx.core.Future;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class BackgroundPoller {
+  private final WebClient client;
 
-  public Future<List<String>> pollServices(Map<String, String> services) {
-    //TODO
-    return Future.failedFuture("TODO");
+  private Logger LOGGER = LoggerFactory.getLogger(BackgroundPoller.class);
+  private static String URL = "url";
+  private static String STATUS = "status";
+  private static String OK = "OK";
+  private static String FAIL = "FAIL";
+  private final Vertx vertx;
+  public BackgroundPoller(Vertx vertx) {
+    this.client = WebClient.create(vertx);
+    this.vertx = vertx;
   }
+
+  private DBConnector dbConnector = new DBConnector(Vertx.vertx());
+
+  public Future<List<JsonObject>> pollServices(List<JsonObject> services) {
+    for(JsonObject service : services) {
+      String url = (String)service.getValue("url");
+      LOGGER.info("Trying to poll %s", url);
+      client.getAbs(url).send(ar -> {
+        if (ar.succeeded()) {
+          service.put(STATUS, OK);
+        } else {
+          LOGGER.info(ar.cause());
+          service.put(STATUS, FAIL);
+        }
+      });
+    }
+    return Future.succeededFuture(services);
+  }
+
+
+
+
 }
